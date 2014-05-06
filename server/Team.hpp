ed7659f97653af690a079c75d58ea9f5d4bc08ed
buret_j <buret_j@epitech.eu>
2014-05-06 14:22:22 +0200

    server: added Team class to namespace Server

--- server/Team.hpp
+++ server/Team.hpp
@@ -5,35 +5,39 @@
 // Login   <buret_j@epitech.net>
 // 
 // Started on  Tue May  6 14:06:52 2014 buret_j
-// Last update Tue May  6 14:14:34 2014 buret_j
+// Last update Tue May  6 14:18:18 2014 buret_j
 //
 
 #ifndef TEAM_HPP_
 # define TEAM_HPP_
 
-class	Team {
+namespace Server {
 
-  size_t _id;
-  size_t _color;
-  size_t _score;
-  size_t _players;
+  class	Team {
 
-public:
-  Team();
-  ~Team();
+    size_t _id;
+    size_t _color;
+    size_t _score;
+    size_t _players;
 
-  inline size_t getID() const { return this->_id; }
-  inline size_t getColor() const { return this->_color; }
-  inline size_t getScore() const { return this->_score; }
+  public:
+    Team();
+    ~Team();
 
-  inline void   cleanScore() { this->_score = 0; }
-  inline void   incrScore() { this->_score += 1; }
-  inline void   decrScore() { this->_score -= 1; }
+    inline size_t getID() const { return this->_id; }
+    inline size_t getColor() const { return this->_color; }
+    inline size_t getScore() const { return this->_score; }
 
-  inline void   addPlayer() { this->_players += 1; }
-  inline void   rmPlayer() { this->_players -= 1; }
-  inline size_t getplayers() const { return this->_players; }
+    inline void   cleanScore() { this->_score = 0; }
+    inline void   incrScore() { this->_score += 1; }
+    inline void   decrScore() { this->_score -= 1; }
 
-};
+    inline void   addPlayer() { this->_players += 1; }
+    inline void   rmPlayer() { this->_players -= 1; }
+    inline size_t getplayers() const { return this->_players; }
+
+  }; // !class Team
+
+}
 
 #endif /* !TEAM_HPP_ */
