696acf4f6952d9ad2ace94019cb0313ee438184c
buret_j <buret_j@epitech.eu>
2014-05-06 14:05:27 +0200

    server: player & server classes improved

--- server/Player.hpp
+++ server/Player.hpp
@@ -5,7 +5,7 @@
 // Login   <buret_j@epitech.net>
 // 
 // Started on  Mon May  5 17:14:40 2014 buret_j
-// Last update Mon May  5 18:47:58 2014 buret_j
+// Last update Tue May  6 14:03:54 2014 buret_j
 //
 
 #ifndef PLAYER_HPP_
@@ -13,22 +13,36 @@
 
 # include <sys/time.h>
 
-# define TIME(t)	gettimeofday(&t, 0)
+# include "Team.hpp"
+
+# define TIME(t)  gettimeofday(&t, 0)
+# define DELAY	  350 // msec
 
 namespace Server {
 
   class Player {
-    size_t	_id;
-    bool	_bot;
-    size_t	_posX, _posY;
+    size_t	 _id;
+    Server::Team *_team;
+    bool	 _bot;
+    size_t	 _posX, _posY;
+
     timeval	_lastCommand;
+    double	_lastCommandTimeMultiplier;// differentiating displacement & orientation commands
+    double	_commandTimeMultiplier;// used by bonus, '1' by default
 
   public:
+    Player();
+    ~Player();
+
     inline bool   isBot() const { return this->_bot; }
     inline size_t getPosX() const { return this->_posX; }
     inline size_t getPosY() const { return this->_posY; }
-    inline size_t getTimeSinceLastCommand() { timeval t; TIME(t); return t.tv_sec-_lastCommand.tv_sec; }
     inline void   updateDateOfLastCommand() { TIME(_lastCommand); }
+    inline size_t getTimeSinceLastCommand() {
+      timeval t; TIME(t);
+      return (t.tv_sec % 1000 * 1000 + t.tv_usec / 1000) -
+	(_lastCommand.tv_sec % 1000 * 1000 + _lastCommand.tv_usec / 1000);
+    }
 
   };// ! class Player
 
--- server/Server.hpp
+++ server/Server.hpp
@@ -5,7 +5,7 @@
 // Login   <buret_j@epitech.net>
 //
 // Started on  Mon May  5 16:51:35 2014 buret_j
-// Last update Mon May  5 18:57:05 2014 buret_j
+// Last update Tue May  6 14:04:31 2014 buret_j
 //
 
 #ifndef SERVER_HPP_
@@ -15,13 +15,26 @@
 # include <map>
 
 # include "Player.hpp"
+# include "Map.hpp"
+# include "Exception.hpp"
 
 namespace Server {
 
   class	Server {
-    std::map<std::pair<size_t, size_t>, int>	*entities;
-    std::map<size_t, Server::Player *>	playersAlive;// size_t is Player's _id attribute
-    std::map<size_t, Server::Player *>	playersDead;
+    std::list<Server::Team *> _teams;
+    std::map<size_t, Server::Player *>	_playersAlive;// size_t is Player's _id attribute
+    std::map<size_t, Server::Player *>	_playersDead;
+
+    Map				*_map;
+    std::list<std::string>	_mapNames;
+
+    void   retrieveConfig();
+    size_t retrieveMapNames();
+
+  public:
+    Server();
+    ~Server();
+
   };
 
 }
