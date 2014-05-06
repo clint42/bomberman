b85a29cf995918acf2339db09afb96f20323b3d8
buret_j <buret_j@epitech.eu>
2014-05-06 17:27:47 +0200

    server: made it compile a bit more

--- server/Player.cpp
+++ server/Player.cpp
@@ -5,14 +5,15 @@
 // Login   <buret_j@epitech.net>
 // 
 // Started on  Tue May  6 12:24:05 2014 buret_j
-// Last update Tue May  6 14:01:44 2014 buret_j
+// Last update Tue May  6 17:16:25 2014 buret_j
 //
 
 #include "Player.hpp"
 
-Player::Player() {
+Server::Player::Player(size_t id, bool bot)
+  : _id(id), _team(0), _bot(bot) {
 }
 
-Player::~Player() {
+Server::Player::~Player() {
 }
 
