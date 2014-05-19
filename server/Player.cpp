7debe484e4869f7b593b0b2a1fd59990a0cc69b6
buret_j <buret_j@epitech.eu>
2014-05-19 18:24:33 +0200

    server: Player missing comma

--- server/Player.cpp
+++ server/Player.cpp
@@ -5,13 +5,13 @@
 // Login   <buret_j@epitech.net>
 // 
 // Started on  Tue May  6 12:24:05 2014 buret_j
-** Last update Mon May 19 17:29:00 2014 lafitt_g
+// Last update Mon May 19 18:23:52 2014 buret_j
 */
 
 #include "Player.hpp"
 
 Server::Player::Player(size_t id, Server::Team *t, bool bot)
-  : _id(id), _bot(bot), _team(t), _posX(0), _posY(0)
+  : _id(id), _bot(bot), _team(t), _posX(0), _posY(0),
     _bombsLimit(1), _bombsOnFloor(0) {
 }
 
