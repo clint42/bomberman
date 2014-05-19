82a4e5ec01e9a99da37fa6caa291de32db7dd4a9
lafitt_g <dedoy@hotmail.fr>
2014-05-19 17:22:38 +0200

    azertyui

--- server/Player.cpp
+++ server/Player.cpp
@@ -1,12 +1,12 @@
-//
+/*
 // Player.cpp for  in /home/buret_j/rendu/bomberman
 // 
 // Made by buret_j
 // Login   <buret_j@epitech.net>
 // 
 // Started on  Tue May  6 12:24:05 2014 buret_j
-// Last update Tue May  6 17:16:25 2014 buret_j
-//
+** Last update Mon May 12 17:21:44 2014 lafitt_g
+*/
 
 #include "Player.hpp"
 
@@ -16,4 +16,3 @@ Server::Player::Player(size_t id, bool bot)
 
 Server::Player::~Player() {
 }
-
