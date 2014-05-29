5492b1ff0e2aded0e287ec8421bcef99f4b87462
prieur_b <prieur_b@epitech.eu>
2014-05-29 17:58:23 +0200

    Nothing

--- common/Mutex.hpp
+++ common/Mutex.hpp
@@ -5,7 +5,7 @@
 // Login   <buret_j@epitech.net>
 // 
 // Started on  Mon Apr 21 14:24:02 2014 buret_j
-// Last update Mon May 26 18:38:59 2014 buret_j
+// Last update Thu May 29 17:56:58 2014 aurelien prieur
 //
 
 #ifndef MUTEX_HH_
@@ -19,15 +19,13 @@ private:
   pthread_mutex_t	_mutex;
 
 public:
-  Mutex(void) { pthread_mutex_init(&this->_mutex, NULL); }
-  ~Mutex(void) { pthread_mutex_destroy(&this->_mutex); }
+  Mutex(void);
+  ~Mutex(void);
 
-  inline bool lock(void) { return pthread_mutex_lock(&this->_mutex) != 0 ? false : true; }
-  inline bool unlock(void) { return pthread_mutex_unlock(&this->_mutex) != 0 ? false : true; }
-  inline bool trylock(void) { return pthread_mutex_trylock(&this->_mutex) == 0 ? true : false; }
-  inline pthread_mutex_t  *getMutex(void) { return &this->_mutex; }
-
-
+  inline bool lock(void);
+  inline bool unlock(void);
+  inline bool trylock(void);
+  inline pthread_mutex_t  *getMutex(void);
 };
 
 #endif
