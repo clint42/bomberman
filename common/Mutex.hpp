d056dd45b73e473256135ec4922fbfe5a055d3ba
prieur_b <prieur_b@epitech.eu>
2014-05-30 11:29:18 +0200

    Remove inline attribute from Mutex method

--- common/Mutex.hpp
+++ common/Mutex.hpp
@@ -5,7 +5,7 @@
 // Login   <buret_j@epitech.net>
 // 
 // Started on  Mon Apr 21 14:24:02 2014 buret_j
-// Last update Thu May 29 17:56:58 2014 aurelien prieur
+// Last update Fri May 30 11:25:49 2014 aurelien prieur
 //
 
 #ifndef MUTEX_HH_
@@ -22,10 +22,10 @@ public:
   Mutex(void);
   ~Mutex(void);
 
-  inline bool lock(void);
-  inline bool unlock(void);
-  inline bool trylock(void);
-  inline pthread_mutex_t  *getMutex(void);
+  bool lock(void);
+  bool unlock(void);
+  bool trylock(void);
+  pthread_mutex_t  *getMutex(void);
 };
 
 #endif
