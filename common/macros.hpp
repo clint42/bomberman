0ed7d6b585265cb2efa78f79cf11371a98675bd4
buret_j <buret_j@epitech.eu>
2014-06-02 13:24:55 +0200

    common: added header macros.hpp to contains all macros

---
+++ common/macros.hpp
@@ -0,0 +1,16 @@
+//
+// macros.hpp for  in /home/buret_j/rendu/bomberman
+// 
+// Made by buret_j
+// Login   <buret_j@epitech.net>
+// 
+// Started on  Mon Jun  2 13:23:08 2014 buret_j
+// Last update Mon Jun  2 13:24:18 2014 buret_j
+//
+
+#ifndef MACROS_HPP_
+# define MACROS_HPP_
+
+# define CVRT_STRING_TO_SIZET(str, res) { std::stringstream ss(str); ss >> res; }
+
+#endif /* !MACROS_HPP_ */
