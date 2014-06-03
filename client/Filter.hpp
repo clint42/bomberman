dea9e67c94aa300956e725a92b64a3b15c941960
lafitt_g <dedoy@hotmail.fr>
2014-06-03 13:28:13 +0200

    [client] Fix error in filter

--- client/Filter.hpp
+++ client/Filter.hpp
@@ -5,7 +5,7 @@
 ** Login   <lafitt_g@lafittg>
 ** 
 ** Started on  Mon Jun  2 18:21:37 2014 lafitt_g
-** Last update Mon Jun  2 18:38:48 2014 lafitt_g
+** Last update Mon Jun  2 18:54:49 2014 lafitt_g
 */
 
 #ifndef FILTER_HPP_
@@ -29,5 +29,8 @@ struct			t_msg
   timeval		_date;
   std::string		_msg;
 };
+
+void		getInformation(const std::string &, size_t *, size_t, size_t);
+t_cmd		*filterCmd(const t_msg &);
 
 #endif /* !FILTER_HPP_ */
