d1772c20528925df76ae6efec4c13144f7197dae
buret_j <buret_j@epitech.eu>
2014-06-03 15:41:34 +0200

    common/

--- common/Exception.cpp
+++ common/Exception.cpp
@@ -5,7 +5,7 @@
 // Login   <franel_j@epitech.net>
 //
 // Started on  Tue May  6 11:18:51 2014 julie franel
-// Last update Mon Jun  2 14:09:29 2014 julie franel
+// Last update Tue Jun  3 15:40:58 2014 buret_j
 //
 
 #include "Exception.hpp"
@@ -29,3 +29,6 @@ SocketException::~SocketException(void) throw() {}
 
 ServerException::ServerException(const std::string &msg) throw(): ABombermanException(msg) {}
 ServerException::~ServerException(void) throw() {}
+
+ConnexionException::ConnexionException(const std::string &m) throw() : ABombermanException(m) {}
+ConnexionException::~ConnexionException() throw() {}
