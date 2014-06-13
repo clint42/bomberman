e305e097fd246e289ae31c10080c2a73e0ebd535
franel_j <franel_j@epitech.eu>
2014-06-13 14:41:53 +0200

    debug in main client

--- client/main.cpp
+++ client/main.cpp
@@ -1,11 +1,11 @@
 //
 // main.cpp for bomberman in /home/prieur_b/tek2/cpp/bomberman/client
-// 
+//
 // Made by aurelien prieur
 // Login   <prieur_b@epitech.net>
-// 
+//
 // Started on  Fri May 16 15:53:47 2014 aurelien prieur
-// Last update Fri Jun 13 11:55:06 2014 aurelien prieur
+// Last update Fri Jun 13 14:25:18 2014 julie franel
 //
 
 #include <cstdlib>
@@ -31,13 +31,23 @@ int	main()
   ConnexionHandler	connexionHandler;
   Signal		signal(&connexionHandler);
   signal.catchSignal(SIGINT, false);
-  
+
   //TODO: Move to the right place. Testing purpose only.
   MenuHandler menuHandler(signal);
   t_game	*options;
 
   if ((options = (menuHandler.launchMenus())) == NULL)
     return (EXIT_FAILURE);
+  std::cout << "=========================> " << std::endl;
+  std::cout << "MAPNAME= " << options->mapName << std::endl;
+  std::cout << "NBPLAYERS= " << options->nbPlayers << std::endl;
+  std::cout << "NBBOTS= " << options->nbBots << std::endl;
+  std::cout << "TIMEGAME= " << options->timeGame << std::endl;
+  std::cout << "IPADDR= " << options->ipAddr << std::endl;
+  std::cout << "SERVERPORT= " << options->serverPort << std::endl;
+  std::cout << "ISHOST= " << std::boolalpha << options->isHost << std::endl;
+  std::cout << "ISDOUBLE= " << std::boolalpha << options->isDouble << std::endl;
+
 
   EventsHandler		eventsHandler(connexionHandler);
   GameEntities		gameEntities;
@@ -45,7 +55,7 @@ int	main()
 
   //3: LAUNCH CORE AND GRAPHICENGINE
   ClientCore	core(gameEntities, eventsHandler, createInstructs, connexionHandler);
-  
+
   if (!core.initialize(options))
     {
       std::cerr << "Initialization failure" << std::endl;
